b079239fd8dc2ab7b5d2b8ed06ddc2be39315cdc
Чинчи Саны-Шири <Sany-ShiriCE@mer.ci.nsu.ru>
2024-06-14 14:29:39 +0700

    Добавьте файлы проекта.

---
+++ ExamGloNo33/DataContext/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using ExamGloNo33.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamGloNo33.DataContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=your_server;Database=your_database;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ExamGloNo33/ViewModels/UserViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using ExamGloNo33.DataContext;
+using ExamGloNo33.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamGloNo33.ViewModels
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<User> _users;
+        private string _filterText;
+
+        public ObservableCollection<User> Users
+        {
+            get => _users;
+            set
+            {
+                _users = value;
+                OnPropertyChanged(nameof(Users));
+            }
+        }
+
+        public string FilterText
+        {
+            get => _filterText;
+            set
+            {
+                _filterText = value;
+                OnPropertyChanged(nameof(FilterText));
+                ApplyFilter();
+            }
+        }
+
+        public UserViewModel()
+        {
+            LoadUsers();
+        }
+
+        private void LoadUsers()
+        {
+            using (var context = new ApplicationContext())
+            {
+                Users = new ObservableCollection<User>(context.Users.Include(u => u.Employee).Include(u => u.Role).ToList());
+            }
+        }
+
+        private void ApplyFilter()
+        {
+            if (string.IsNullOrWhiteSpace(FilterText))
+            {
+                LoadUsers();
+            }
+            else
+            {
+                using (var context = new ApplicationContext())
+                {
+                    Users = new ObservableCollection<User>(
+                        context.Users.Include(u => u.Employee).Include(u => u.Role)
+                        .Where(u => u.Login.Contains(FilterText) || u.Employee.FullName.Contains(FilterText))
+                        .ToList());
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
